e39a9c68e696dcb8230554ccf3bf23f0337a31f4
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-02-04 14:35:15 +0000

    punto 4

--- form-auto/src/app/app.module.ts
+++ form-auto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CartListComponent } from './cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule
